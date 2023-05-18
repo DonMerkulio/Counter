ca8e39abe424b9a0ab1a38249427bda7c73bd73e
Aleksandr Merkulov <user.aleksandrmerkylov@gmail.com>
2023-05-18 11:50:58 +0300

    добавил: OutLet кнопки сброса и текстового поля истории изменений изменил: Вынес изменение кнопок в отдельный метод поправил описание

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -13,16 +13,20 @@ class ViewController: UIViewController {
     
     // outlet отображения счетчика
     @IBOutlet weak var scoreLabel: UILabel!
+    // outlet отображения истории изменений
+    @IBOutlet weak var historyText: UITextView!
+    
     // outlet кнопки увеличения значения
     @IBOutlet weak var addButton: UIButton!
     // outlet кнопки уменьшения значения
     @IBOutlet weak var subtractButton: UIButton!
-    
+    // outlet кнопки сброса значения
+    @IBOutlet weak var resetButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //MARK: Выделение score жирным и увеличения size font
+        //MARK: LABEL счетчик
+        //Выделение "score" жирным и увеличения size font
         let attributedText = NSMutableAttributedString(string: "Значение счётчика: \(score)")
         let boldAttributes: [NSAttributedString.Key: Any] = [
             .font: UIFont.boldSystemFont(ofSize: 22)
@@ -30,18 +34,30 @@ class ViewController: UIViewController {
         let scoreRange = NSRange(location: 19, length: "\(score)".count)
         attributedText.addAttributes(boldAttributes, range: scoreRange)
         
-        //MARK: Добавление форматированного текста в lable
+        // Добавление форматированного текста в lable
         scoreLabel.attributedText = attributedText
-        //MARK: Округление краев lable
+        // Округление краев lable
         scoreLabel.layer.cornerRadius = 10
         scoreLabel.clipsToBounds = true
         
-        //MARK: Настройка отображения кнопки добавления
-        addButton.titleLabel?.font = UIFont.systemFont(ofSize: 40)
-        addButton.layer.cornerRadius = 15
-        //MARK: Настройка отображения кнопки уменьшения
-        subtractButton.titleLabel?.font = UIFont.systemFont(ofSize: 40)
-        subtractButton.layer.cornerRadius = 15
+        //MARK: TextView
+        
+        
+        
+        
+        //MARK: КНОПКИ
+        // изменение внешнего вида у кнопок увеличения и уменьшения
+        addButton = changingApperanceButton(of: addButton)
+        subtractButton = changingApperanceButton(of: subtractButton)
+        resetButton.layer.cornerRadius = 20
+        
+    }
+    
+    //MARK: метод для изменения отображения кнопки (размер текста, округление формы)
+    private func changingApperanceButton(of button: UIButton!) -> UIButton! {
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 40)
+        button.layer.cornerRadius = 20
+        return button
     }
 }
 
